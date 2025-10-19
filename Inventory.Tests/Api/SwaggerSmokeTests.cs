1f4272fcc498a2c3c41e045653740080e91714d3
ritaademi <rita.ademi19@gmail.com>
2025-10-19 18:15:19 +0200

    add xUnit unit tests + minimal API integration tests

---
+++ Inventory.Tests/Api/SwaggerSmokeTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Tests.Api
+{
+    public class SwaggerSmokeTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public SwaggerSmokeTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task Swagger_endpoint_is_up()
+        {
+            var res = await _client.GetAsync("/swagger");
+            res.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
